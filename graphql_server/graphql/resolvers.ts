f899aca768542382389ddd97c5214a70265cab4a
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-25 22:13:59 +0900

    Search movie

--- graphql_server/graphql/resolvers.ts
+++ graphql_server/graphql/resolvers.ts
@@ -1,8 +1,9 @@
-import { getMovies } from "./db"
+import { getMovies, getMovie } from "./db"
 
 const resolvers = {
   Query: {
     movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
   },
 }
 
