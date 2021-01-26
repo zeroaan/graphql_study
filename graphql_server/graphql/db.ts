41846423877aa2358713119a874d1f2b68aba721
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-26 21:26:29 +0900

    Update: graphql server

--- graphql_server/graphql/db.ts
+++ graphql_server/graphql/db.ts
@@ -3,6 +3,7 @@ import axios from "axios"
 const BASE_URL = "https://yts.mx/api/v2/"
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json?`
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json?`
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`
 
 export const getMovies = async (limit: number, rating: number) => {
   const {
@@ -29,6 +30,19 @@ export const getMovie = async (id: number) => {
     },
   })
   return movie
+}
+
+export const getSuggestions = async (id: number) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
+    },
+  })
+  return movies
 }
 
 /*
