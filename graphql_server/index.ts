93fb743bc5c22609cc02efe4d014ffd3a390826b
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-27 22:08:20 +0900

    change graphql-yoga -> apollo server

--- graphql_server/index.ts
+++ graphql_server/index.ts
@@ -1,9 +1,26 @@
-import { GraphQLServer } from "graphql-yoga"
+import { ApolloServer, gql } from "apollo-server"
 import resolvers from "./graphql/resolvers"
 
-const server = new GraphQLServer({
-  typeDefs: "graphql/schema.graphql",
-  resolvers,
-})
+const typeDefs = gql`
+  type Movie {
+    id: Int!
+    title: String!
+    rating: Float
+    description_intro: String
+    language: String
+    medium_cover_image: String
+    genres: [String]
+  }
 
-server.start(() => console.log("Graphql Server Running..."))
+  type Query {
+    movies(limit: Int, rating: Float): [Movie]!
+    movie(id: Int!): Movie
+    suggestions(id: Int!): [Movie]!
+  }
+`
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
