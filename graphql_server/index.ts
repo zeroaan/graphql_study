a7eb7070345a8f7d72bac19085805b023fcb2794
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-27 22:19:58 +0900

    typeDefs

--- graphql_server/index.ts
+++ graphql_server/index.ts
@@ -1,23 +1,7 @@
-import { ApolloServer, gql } from "apollo-server"
+import { ApolloServer } from "apollo-server"
+
+import typeDefs from "./graphql/typeDefs"
 import resolvers from "./graphql/resolvers"
-
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    rating: Float
-    description_intro: String
-    language: String
-    medium_cover_image: String
-    genres: [String]
-  }
-
-  type Query {
-    movies(limit: Int, rating: Float): [Movie]!
-    movie(id: Int!): Movie
-    suggestions(id: Int!): [Movie]!
-  }
-`
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
